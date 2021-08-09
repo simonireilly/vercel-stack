09a3611197f04ca41cb0de131b16c87194bf2f12
simon <simonireilly@gmail.com>
2021-08-09 21:05:37 +0100

    Get callback urls from vercel v0.1.1

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -1,7 +1,4 @@
-export const callbackUrls = (
-  vercelProjectName: string,
-  kebabBranchName: string
-) => {
+export const callbackUrls = (vercelProjectName: string) => {
   if (process.env.IS_LOCAL) {
     return {
       callbackUrls: [`http://localhost:3000/api/auth/callback/cognito`],
@@ -11,14 +8,20 @@ export const callbackUrls = (
 
   return {
     callbackUrls: [
-      `https://${vercelProjectName}-git-${getBranchName().replace(/\//g, '-')}-simonireilly.vercel.app/api/auth/callback/cognito`,
+      `https://${vercelProjectName}-git-${getBranchName().replace(
+        /\//g,
+        '-'
+      )}-simonireilly.vercel.app/api/auth/callback/cognito`,
     ],
     logoutUrls: [
-      `https://${vercelProjectName}-git-${getBranchName().replace(/\//g, '-')}-simonireilly.vercel.app`,
+      `https://${vercelProjectName}-git-${getBranchName().replace(
+        /\//g,
+        '-'
+      )}-simonireilly.vercel.app`,
     ],
   };
 };
 
 const { execSync } = require('child_process');
 const getBranchName = (): string =>
-  execSync('git rev-parse --abbrev-ref HEAD').toString().;
+  execSync('git rev-parse --abbrev-ref HEAD').toString();
